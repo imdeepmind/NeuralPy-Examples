11666a85db8b282b2f4c9f93fc725cabc18457fb
jeffin07 <francisjeffin07@gmail.com>
2021-03-16 23:44:33 +0530

    dataset added error in fit

--- alexnet/model.py
+++ alexnet/model.py
@@ -7,9 +7,13 @@ from neuralpy.layers.other import Flatten
 from neuralpy.loss_functions import CrossEntropyLoss
 from neuralpy.optimizer import SGD
 
+import torch
+import torchvision.datasets as datasets
+import torchvision.transforms as transforms
+
 model = Sequential()
 
-model.add(Conv2D(input_shape=(1,28,28), filters=96, kernel_size=11, stride=4))
+model.add(Conv2D(input_shape=(3,224,224), filters=96, kernel_size=11, stride=4))
 model.add(ReLU())
 model.add(MaxPool2D(kernel_size=3, stride=2))
 model.add(ReLU())
@@ -38,3 +42,19 @@ model.add(Dense(n_nodes=10))
 model.build()
 model.compile(optimizer=SGD(), loss_function=CrossEntropyLoss(), metrics=["accuracy"])
 print(model.summary())
+
+train_data = datasets.CIFAR100(root="datset/",train=True,download=True, transform=transforms.Compose([transforms.CenterCrop(224),transforms.ToTensor()]))
+train_loader = torch.utils.data.DataLoader(train_data, batch_size=32)
+
+test_data = datasets.CIFAR100(root="datset/",train=False,download=True, transform=transforms.Compose([transforms.CenterCrop(224),transforms.ToTensor()]))
+test_loader = torch.utils.data.DataLoader(test_data, batch_size=2)
+
+next_data_sample = next(iter(train_loader))
+print(next_data_sample[1].shape, next_data_sample[1].shape)
+
+exit()
+
+train_samples_imgs = next_data_sample[0][0:500]
+train_samples_lbs = next_data_sample[1][0:500]
+
+model.fit(train_data=(train_samples_imgs,train_samples_lbs), epochs=5, validation_data=(train_samples_imgs,train_samples_lbs),batch_size=2)
