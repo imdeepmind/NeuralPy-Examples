b09a599192b830ff87a72b4fad3a4693067c8e52
jeffin07 <francisjeffin07@gmail.com>
2021-03-17 23:12:08 +0530

    model ready with mnist need to test on other dataset and settings

--- alexnet/model.py
+++ alexnet/model.py
@@ -5,7 +5,7 @@ from neuralpy.layers.activation_functions import ReLU,Softmax
 from neuralpy.layers.pooling import MaxPool2D
 from neuralpy.layers.other import Flatten
 from neuralpy.loss_functions import CrossEntropyLoss
-from neuralpy.optimizer import SGD
+from neuralpy.optimizer import SGD,Adam
 
 import torch
 import torchvision.datasets as datasets
@@ -13,7 +13,7 @@ import torchvision.transforms as transforms
 
 model = Sequential()
 
-model.add(Conv2D(input_shape=(3,224,224), filters=96, kernel_size=11, stride=4))
+model.add(Conv2D(input_shape=(1,224,224), filters=96, kernel_size=11, stride=4))
 model.add(ReLU())
 model.add(MaxPool2D(kernel_size=3, stride=2))
 model.add(ReLU())
@@ -40,21 +40,44 @@ model.add(Dense(n_nodes=10))
 # model.add(Softmax()))
 
 model.build()
-model.compile(optimizer=SGD(), loss_function=CrossEntropyLoss(), metrics=["accuracy"])
+model.compile(optimizer=Adam(), loss_function=CrossEntropyLoss(), metrics=["accuracy"])
 print(model.summary())
 
-train_data = datasets.CIFAR100(root="datset/",train=True,download=True, transform=transforms.Compose([transforms.CenterCrop(224),transforms.ToTensor()]))
-train_loader = torch.utils.data.DataLoader(train_data, batch_size=32)
+# train_data = datasets.CIFAR100(root="datset/",train=True,download=True, transform=transforms.Compose([transforms.CenterCrop(224),transforms.ToTensor()]))
+# train_loader = torch.utils.data.DataLoader(train_data, batch_size=32)
 
-test_data = datasets.CIFAR100(root="datset/",train=False,download=True, transform=transforms.Compose([transforms.CenterCrop(224),transforms.ToTensor()]))
-test_loader = torch.utils.data.DataLoader(test_data, batch_size=2)
+# test_data = datasets.CIFAR100(root="datset/",train=False,download=True, transform=transforms.Compose([transforms.CenterCrop(224),transforms.ToTensor()]))
+# test_loader = torch.utils.data.DataLoader(test_data, batch_size=32)
 
-next_data_sample = next(iter(train_loader))
-print(next_data_sample[1].shape, next_data_sample[1].shape)
+# next_data_sample = next(iter(train_loader))
+# print(next_data_sample[0][0].shape, next_data_sample[1][0])
 
-exit()
+# train_samples_imgs = next_data_sample[0]
+# train_samples_lbs = next_data_sample[1]
 
-train_samples_imgs = next_data_sample[0][0:500]
-train_samples_lbs = next_data_sample[1][0:500]
+# print(train_samples_imgs.shape, train_samples_lbs.shape)
 
-model.fit(train_data=(train_samples_imgs,train_samples_lbs), epochs=5, validation_data=(train_samples_imgs,train_samples_lbs),batch_size=2)
+
+train_set = datasets.MNIST(
+    root='./data'
+    ,train=True
+    ,download=True
+    ,transform=transforms.Compose([
+        transforms.CenterCrop(224),
+        transforms.ToTensor()
+    ])
+)
+
+# Load the dataset from pytorch's Dataloader function
+train_loader  = torch.utils.data.DataLoader(train_set, batch_size=1000)
+#Get the data
+mnist_data = next(iter(train_loader))
+#Split into train and test set
+train_imgs = mnist_data[0][:500]
+train_labels = mnist_data[1][:500]
+test_imgs = mnist_data[0][500:]
+test_labels = mnist_data[1][500:]
+
+
+
+model.fit(train_data=(train_imgs,train_labels), epochs=5, validation_data=(test_imgs,test_labels),batch_size=1)
